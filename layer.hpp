23c08a1cd3ddcec7ffad907d0422efe7202c6159
mutsuki <mutsuki@momo86.net>
2017-05-20 18:40:39 +0900

    順伝播完成

--- layer.hpp
+++ layer.hpp
@@ -1,24 +1,52 @@
+#pragma once
 #include "Eigen/Dense"
 
-template<class Func>
+template<class ActivateFunc,class dActivateFunc>
 class Layer{
 	int input_size,output_size;
-	Eigen::MatrixXf w1;
-	Eigen::VectorXf b1;
-	Eigen::VectorXf u1;
-	Eigen::VectorXf z0;
+	int batch_size;
+	Eigen::MatrixXf  w1;
+	Eigen::MatrixXf  dw1;
+	Eigen::MatrixXf  rdw1; // round d
+	Eigen::MatrixXf  b1;
+	Eigen::MatrixXf  db1;
+	Eigen::MatrixXf  rdb1; // round d
+	Eigen::MatrixXf  u1;
+	Eigen::MatrixXf  z0;
+	Eigen::MatrixXf  d1;
 public:
-	Layer(int input_size,int output_size):
-		input_size(input_size),output_size(output_size)
+	Layer(int input_size,int output_size,int batch_size):
+		input_size(input_size),output_size(output_size),batch_size(batch_size)
 	{
-		z0 = Eigen::VectorXf(input_size);
+		z0 = Eigen::MatrixXf(input_size,batch_size);
 		w1 = Eigen::MatrixXf::Random(output_size,input_size);
-		b1 = Eigen::VectorXf::Random(output_size);
-		u1 = Eigen::VectorXf(output_size);
+		dw1 = Eigen::MatrixXf::Random(output_size,input_size);
+		b1 = Eigen::MatrixXf::Random(output_size,1);
+		db1 = Eigen::MatrixXf::Random(output_size,batch_size);
+		u1 = Eigen::MatrixXf(output_size,batch_size);
 	}
-	Eigen::VectorXf forwardPropagate(Eigen::VectorXf input){
+	Eigen::MatrixXf forwardPropagate(const Eigen::MatrixXf& input){
 		z0 = input;
-		u1 = w1 * z0 + b1;
-		return u1.unaryExpr(Func());
+		u1 = w1 * z0 + b1 * Eigen::MatrixXf::Constant(1,batch_size,1.0f);
+		return  u1.unaryExpr(ActivateFunc());
+	}
+	Eigen::MatrixXf backPropagate(const Eigen::MatrixXf& d2,const Eigen::MatrixXf& w2){
+		d1 = u1.unaryExpr( dActivateFunc() ).array() * (w2.transpose()*d2).array();
+		rdw1 = d1*z0.transpose()/static_cast<float>(batch_size);
+		rdb1 = d1/static_cast<float>(batch_size);
+		return d1;
+	}
+	void reflect(){
+		const float lerning_rate = 0.03f;
+		dw1 = rdw1 * (-lerning_rate);
+		db1 = rdb1 * (-lerning_rate);
+		w1 = w1 + dw1;
+		b1 = b1 + db1;
+	}
+	void setD2(const Eigen::MatrixXf& d2){
+		d1 = d2;
+	}
+	Eigen::MatrixXf getW() const{
+		return w1;
 	}
 };
--- main.cpp
+++ main.cpp
@@ -1,14 +1,43 @@
 #include "layer.hpp"
+#include <iostream>
 #include <cmath>
 
 
+const int input_size = 4;
+const int layer0_output_size = 8;
+const int layer1_output_size = 1;
+const int batch_size = 10;
+const int calc = 1000;
+
 class Sigmoid{
 public:
-	float operator()(float x){
+	float operator()(const float x) const{
 		return 1.0f/(1.0f+std::exp(-x));
 	}
 };
+class dSigmoid{
+public:
+	float operator()(const float x) const{
+		float s = Sigmoid()(x);
+		return s * (1.0f-s);
+	}
+};
+
+void initLearningDataset(Eigen::MatrixXf &batch_input,Eigen::MatrixXf &batch_teacher){
+	for(int b = 0; b < batch_size;b++){
+
+	}
+}
 
 int main(){
-	Layer<Sigmoid> layer(10,10);
+	Eigen::MatrixXf batch_input = Eigen::MatrixXf::Random(input_size,batch_size);
+	Eigen::MatrixXf batch_teacher = Eigen::MatrixXf::Random(layer1_output_size,batch_size);
+	Layer<Sigmoid,dSigmoid> layer0(input_size,layer0_output_size,batch_size);
+	Layer<Sigmoid,dSigmoid> layer1(layer0_output_size,layer1_output_size,batch_size);
+	for(int c = 0;c < calc;c++){
+		initLearningDataset(batch_input,batch_teacher);
+		auto layer0_out = layer0.forwardPropagate(batch_input);
+		auto layer1_out = layer1.forwardPropagate(layer0_out);
+		std::cout<<"out="<<layer1_out<<std::endl;
+	}
 }
