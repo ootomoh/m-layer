074ed877d4cc73157f3109826e4b8569041c8ec7
mutsuki <mutsuki@momo86.net>
2017-05-20 19:37:38 +0900

    ad  入出力値表示制御define

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <algorithm>
 #include <random>
 
+
+#define SHOW_INPUT
+#define SHOW_OUTPUT
+
 const int input_size = 4;
 const int layer0_output_size = 8;
 const int layer1_output_size = 1;
@@ -38,8 +42,6 @@ void initLearningDataset(Eigen::MatrixXf &batch_input,Eigen::MatrixXf &batch_tea
 		else
 			batch_teacher(0,b)=0.0f;
 	}
-	std::cout<<"input="<<std::endl<<batch_input<<std::endl;
-	std::cout<<"teacher="<<std::endl<<batch_teacher<<std::endl;
 }
 
 int main(){
@@ -49,10 +51,16 @@ int main(){
 	Layer<Sigmoid,dSigmoid> layer1(layer0_output_size,layer1_output_size,batch_size);
 	for(int c = 0;c < calc;c++){
 		initLearningDataset(batch_input,batch_teacher);
+#ifdef SHOW_INPUT
+		std::cout<<"input="<<std::endl<<batch_input<<std::endl;
+		std::cout<<"teacher="<<std::endl<<batch_teacher<<std::endl;
+#endif
 		auto layer0_out = layer0.forwardPropagate(batch_input);
 		auto layer1_out = layer1.forwardPropagate(layer0_out);
 		auto error = layer1_out - batch_teacher;
+#ifdef SHOW_OUTPUT
 		std::cout<<"out="<<layer1_out<<std::endl;
 		std::cout<<"err="<<error<<std::endl;
+#endif
 	}
 }
