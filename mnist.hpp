3c7c3f90cfb33d9806e54af97ae03869fbb5b099
mutsuki <mutsuki@momo86.net>
2017-06-08 08:55:25 +0900

    add mnist data

--- mnist.hpp
+++ mnist.hpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <string>
 #include "Eigen/Core"
 
 namespace mtk{
 	class MNISTAnalizer{
+		class MNISTData{
+			float data[28];
+			int label;
+		};
+		int reverse(int n);
 	public:
-
+		MNISTAnalizer(){}
+		int setToMatrix(Eigen::MatrixXf& input,int batch_size);
+		int loadMNISTData(std::string image_filename,std::string label_filename);
 	}
-}
+};
