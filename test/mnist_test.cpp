5b202d10ec168aa7bc3817559624bd07a0f469de
mutsuki <mutsuki@momo86.net>
2017-06-08 10:41:56 +0900

    型拡張した際に1埋めされるのがつらかったけどなんとか(無理やり)直した(´・ω・｀)

---
+++ test/mnist_test.cpp
@@ -0,0 +1,20 @@
+#include "../mnist.hpp"
+
+int main(){
+	const int batch_size = 10;
+	const int input_size = 28*28;
+	const int teacher_size = 10;
+	Eigen::MatrixXf input(input_size,batch_size);
+	Eigen::MatrixXf teacher(teacher_size,batch_size);
+
+	mtk::MNISTAnalizer mnist;
+	if(mnist.loadMNISTData("../train-images-idx3-ubyte","../train-labels-idx1-ubyte")){
+		std::cerr<<"Loading error"<<std::endl;
+		return 1;
+	}
+
+	mnist.setToMatrix(input,teacher,batch_size);
+
+	std::cout<<"input"<<std::endl<<input<<std::endl;
+	std::cout<<"teacher"<<std::endl<<teacher<<std::endl;
+}
